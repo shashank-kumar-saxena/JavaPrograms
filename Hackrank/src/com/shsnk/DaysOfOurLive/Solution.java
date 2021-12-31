3b040a29a90fc965464ea9b4161124f4a6afd870
Shashank <shashanksaxena226@gmail.com>
2021-12-31 18:26:51 +0530

    changes

--- Hackrank/src/com/shsnk/DaysOfOurLive/Solution.java
+++ Hackrank/src/com/shsnk/DaysOfOurLive/Solution.java
@@ -1,5 +1,12 @@
 package com.shsnk.DaysOfOurLive;
 
+import java.util.Scanner;
+
 public class Solution {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),k= sc.nextInt();
+        int []a=new int[7];
+        for(int )
+    }
 }
