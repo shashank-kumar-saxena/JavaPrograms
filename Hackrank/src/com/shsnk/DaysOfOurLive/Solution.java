70060e9baa2b2defa30b7de81a6ae1fdab0cbdb3
Shashank <shashanksaxena226@gmail.com>
2022-01-02 10:50:49 +0530

    daysOfOurLiveProgramAdded

--- Hackrank/src/com/shsnk/DaysOfOurLive/Solution.java
+++ Hackrank/src/com/shsnk/DaysOfOurLive/Solution.java
@@ -5,8 +5,23 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt(),k= sc.nextInt();
-        int []a=new int[7];
-        for(int )
+        int n=sc.nextInt(),k=sc.nextInt();
+        int d=n/7;
+        int []a= {n/7,n/7,n/7,n/7,n/7,n/7,n/7};
+         d=n%7;
+        while(d>0)
+        {
+            a[k-1]+=1;
+            if(k==7)
+            {
+                k=1;
+            }
+            k++;
+            d--;
+        }
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
     }
 }
