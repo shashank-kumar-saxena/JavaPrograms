bc53ba5c1d5f16d360bfef1f1672b3e1603c0bb6
Shashank <shashanksaxena226@gmail.com>
2022-01-02 10:58:46 +0530

    Changes

--- Hackrank/src/com/shsnk/mindGame/Solution.java
+++ Hackrank/src/com/shsnk/mindGame/Solution.java
@@ -2,7 +2,7 @@ package com.shsnk.mindGame;
 
 import java.util.Scanner;
 
-public class MindGame {
+public class Solution {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int n=sc.nextInt();
