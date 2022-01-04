bec41b596a05dec48b0a591e4274f2fd75cfd80c
Shashank <shashanksaxena226@gmail.com>
2022-01-04 22:13:18 +0530

    Reformate

--- Hackrank/src/com/shsnk/smallestDivisibleNumber/Solution.java
+++ Hackrank/src/com/shsnk/smallestDivisibleNumber/Solution.java
@@ -4,30 +4,26 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int []a=new int[n];
-        for(int i=0;i<n;i++)
-        {
-            a[i]=i+1;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = i + 1;
+
         }
-        int s=n,c=1;
-        while(true)
-        {
-            for(int j=0;j<a.length;j++)
-            {
-                if(s%a[j]!=0)
-                {
-                    c=0;
+        int s = n, c = 1;
+        while (true) {
+            for (int j = 0; j < a.length; j++) {
+                if (s % a[j] != 0) {
+                    c = 0;
                     break;
                 }
             }
-            if(c==1)
-            {
+            if (c == 1) {
                 System.out.println(s);
                 break;
             }
-            c=1;
+            c = 1;
             s++;
         }
 
