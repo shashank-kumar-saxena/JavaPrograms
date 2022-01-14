d03681957d45c0a7703d07fc93ca759f8b237024
Shashank <shashanksaxena226@gmail.com>
2022-01-14 13:11:17 +0530

    easyStringProgramAdded

---
+++ Hackrank/src/com/shsnk/EasyString4/Solution.java
@@ -0,0 +1,31 @@
+package com.shsnk.EasyString4;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String st="",s=sc.next();
+        int count=1;
+        s=s+" ";
+        for(int i=1;i<s.length();i++)
+        {
+            if(s.charAt(i-1)==s.charAt(i)||(((int)(s.charAt(i-1)))-32)==s.charAt(i)||(((int)(s.charAt(i)))-32)==s.charAt(i-1))
+            {
+                count++;
+            }
+            else
+            {
+                st=st+s.charAt(i-1);
+                st=st+Integer.toString(count);
+                count=1;
+            }
+
+        }
+        System.out.println(st);
+    }
+}
---
+++ Hackrank/src/com/shsnk/winnerOfAnElection/Solution.java
@@ -0,0 +1,16 @@
+package com.shsnk.winnerOfAnElection;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String []a=new String[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.next();
+        }
+
+    }
+}
