75bb1beb850d1afa67e45301ec5d585ce20e87cd
Shashank <shashanksaxena226@gmail.com>
2022-01-15 00:56:26 +0530

    hackerrenk prgram added

---
+++ Hackrank/src/com/shsnk/minimalMoveToFormString/Solution.java
@@ -0,0 +1,11 @@
+package com.shsnk.minimalMoveToFormString;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String st=sc.next();
+        
+    }
+}
--- Hackrank/src/com/shsnk/winnerOfAnElection/Solution.java
+++ Hackrank/src/com/shsnk/winnerOfAnElection/Solution.java
@@ -6,11 +6,30 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        String s="";
         String []a=new String[n];
         for(int i=0;i<n;i++)
         {
             a[i]=sc.next();
         }
-
+        int max=0,count=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(a[i].equals(a[j]))
+                {
+                    count++;
+                }
+            }
+            System.out.println(count);
+            if(max<count)
+            {
+                s=a[i];
+                max=count;
+            }
+            count=0;
+        }
+        System.out.println(s);
     }
 }
