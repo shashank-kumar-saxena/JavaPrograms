77b46c809abf067feddee22c44decce16f33f5a3
Shashank <shashanksaxena226@gmail.com>
2022-01-31 19:36:11 +0530

    Revert "The Chocolate Problem Of KodeKar event added"
    This reverts commit 824d5cd56d6066226d3e69b18648b94619808632.

--- HackerrankPg2/src/com/shshnk/MinimumDifferencePair1/Solution.java
+++ HackerrankPg2/src/com/shshnk/MinimumDifferencePair1/Solution.java
@@ -4,5 +4,8 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int []arr=new int [105];
+        for(int )
     }
 }
