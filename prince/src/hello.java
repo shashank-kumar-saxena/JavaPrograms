ccc3ad36ad134c224b69f77a669852b3a1fc6e77
Shashank <shashanksaxena226@gmail.com>
2022-02-08 16:51:18 +0530

    program added

---
+++ prince/src/hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class hello
+{
+    public static void main(String []args)
+    {
+        int a,b,c;
+        Scanner sk=new Scanner(System.in);
+        a=sk.nextInt();
+        b=sk.nextInt();
+        c=a+b;
+        System.out.println(c);
+        System.out.println(a+b);
+    }
+}
