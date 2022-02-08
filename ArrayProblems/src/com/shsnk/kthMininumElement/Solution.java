0d147fcea5726941a746bacebfc8c7f450f7b28d
Shashank <shashanksaxena226@gmail.com>
2022-02-08 20:09:13 +0530

    Hackerrank program added

---
+++ ArrayProblems/src/com/shsnk/kthMininumElement/Solution.java
@@ -0,0 +1,34 @@
+/*
+* Given an array arr[] and an integer K where K is smaller than size of array, the task is to find the Kth smallest element in
+*  the given array. It is given that all array elements are distinct.
+* Input:
+N = 6
+arr[] = 7 10 4 3 20 15
+K = 3
+Output : 7
+Explanation :
+3rd smallest element in the given
+array is 7.
+* Input:
+N = 5
+arr[] = 7 10 4 20 15
+K = 4
+Output : 15
+Explanation :
+4th smallest ele*/
+package com.shsnk.kthMininumElement;
+
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
+        
+    }
+}
--- HackerrankPg2/src/com/shshnk/MinimumDifferencePair1/Solution.java
+++ HackerrankPg2/src/com/shshnk/MinimumDifferencePair1/Solution.java
@@ -1,9 +1,23 @@
 package com.shshnk.MinimumDifferencePair1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String st[]=s.split(" ");
+        Arrays.sort(st);
+        int min=Integer.parseInt(st[0])-Integer.parseInt(st[1])+100;
+        for(int i=1;i<st.length;i++)
+        {
+            int a=Integer.parseInt(st[i])-Integer.parseInt(st[i-1]);
+            if(min>=a)
+            {
+                min=a;
+            }
+        }
+        System.out.println(min);
     }
 }
