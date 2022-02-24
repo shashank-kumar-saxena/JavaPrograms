95bf3700115905595caca47f0bd45a7f6d828d4f
Shashank <shashanksaxena226@gmail.com>
2022-02-24 08:28:55 +0530

    ,,,

--- HelloWorld/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-public class HelloWorld {
-    public static void main(String[] args) {
-      //  System.out.println("Hello");
-        float a=(float)4.3;
-        double b=4.2;
-        System.out.println(a+" "+b);
-    }
-}
