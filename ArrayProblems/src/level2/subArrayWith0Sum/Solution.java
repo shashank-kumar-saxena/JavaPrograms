054989bbafa10f56720f999236a28439db4ac637
Shashank <shashanksaxena226@gmail.com>
2022-03-17 10:12:09 +0530

    subArrayWithZeroSum

--- ArrayProblems/src/level2/subArrayWith0Sum/Solution.java
+++ ArrayProblems/src/level2/subArrayWith0Sum/Solution.java
@@ -1,4 +1,7 @@
 package level2.subArrayWith0Sum;
+
+import java.util.Scanner;
+
 /*
 * Given an array of positive and negative numbers. Find if there is a subarray (of size at-least one) with 0 sum.
 *
@@ -26,5 +29,40 @@ package level2.subArrayWith0Sum;
     in the array so there exist a
     subarray with sum 0.*/
 public class Solution {
-
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int c=-1;
+        String st="";
+        boolean b=false;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                for(int k=i;k<j;k++)
+                {
+                    c=c+a[k];
+                    st=st+Integer.toString(a[k])+" ";
+                }
+                c++;
+                if(c==0&&st.length()>0)
+                {
+                    b=true;
+                    System.out.println(st);
+                    break;
+                }
+                c=-1;
+                st="";
+            }
+            if(b)
+                break;
+        }
+        if(!b)
+            System.out.println(-1);
+    }
 }
