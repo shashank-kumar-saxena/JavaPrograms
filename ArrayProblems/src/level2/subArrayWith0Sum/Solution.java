eb524834f7b004101890a9865b0639aed0093b3c
Shashank <shashanksaxena226@gmail.com>
2022-03-18 20:38:03 +0530

    changes

--- ArrayProblems/src/level2/subArrayWith0Sum/Solution.java
+++ ArrayProblems/src/level2/subArrayWith0Sum/Solution.java
@@ -44,7 +44,7 @@ public class Solution {
         {
             for(int j=i+1;j<n;j++)
             {
-                for(int k=i;k<j;k++)
+                for(int k=i;k<=j;k++)
                 {
                     c=c+a[k];
                     st=st+Integer.toString(a[k])+" ";
