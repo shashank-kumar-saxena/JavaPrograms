23ddd81c985dde014082fafd7701d5dd64e1ccd6
Shashank <shashanksaxena226@gmail.com>
2022-04-04 00:55:20 +0530

    QuestionAdded

---
+++ ArrayProblems/src/level2/LongestConsecutiveSubsequence/Solution.java
@@ -0,0 +1,30 @@
+package level2.LongestConsecutiveSubsequence;
+
+import java.util.Scanner;
+/*
+* Given an array of positive integers. Find the length of the longest sub-sequence such that elements in the subsequence are consecutive integers, the consecutive numbers can be in any order.
+
+ * Input:
+N = 7
+a[] = {2,6,1,9,4,5,3}
+Output:
+6
+Explanation:
+The consecutive numbers here
+are 1, 2, 3, 4, 5, 6. These 6
+numbers form the longest consecutive
+subsquence.
+*
+* Input:
+N = 7
+a[] = {1,9,3,10,4,20,2}
+Output:
+4
+Explanation:
+1, 2, 3, 4 is the longest
+consecutive subsequence.*/
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    }
+}
