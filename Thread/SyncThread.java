1a25d59166889e42318bb00a11dad7f0613709fb
Shashank <shashanksaxena226@gmail.com>
2022-06-11 15:58:16 +0530

    Thread

---
+++ Thread/SyncThread.java
@@ -0,0 +1,37 @@
+
+public class SyncThread {
+    static int count=0;
+    public static void main(String[] args) throws InterruptedException {
+        SyncThread p=new SyncThread();
+        Thread t=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0;i<2000;i++)
+                {
+                    p.incre();
+                }
+            }
+        });
+        Thread t2=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0;i<2000;i++)
+                {
+                    p.incre();
+                }
+            }
+        },"Hello");
+        t.start();
+
+        t2.start();
+        t.join();
+        t2.join();;
+        System.out.println(count);
+        System.out.println(t2.getName());
+    }
+    public  void incre(){
+        synchronized (this) {
+            count++;
+        }
+    }
+}
