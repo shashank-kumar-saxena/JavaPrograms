5c7ac66a1c572264dd01913428563c9d117bd146
Shashank <shashanksaxena226@gmail.com>
2022-06-11 15:59:31 +0530

    TopLevelFuncAccessUsingJava

---
+++ FunctionEveryWhere/src/TopLevelFunction/FunContain.kt
@@ -0,0 +1,6 @@
+package TopLevelFunction
+
+fun add(a:Int,b:Int)
+{
+    println("The Addition Using Kotlin Top level Function is "+(a+b))
+}
---
+++ FunctionEveryWhere/src/TopLevelFunction/Solution.java
@@ -0,0 +1,6 @@
+package TopLevelFunction;
+public class Solution {
+    public static void main(String[] args) {
+        FunContainKt.add(10,20);
+    }
+}
