1ceb1f8914de22c9ae36714b2101ae9d5a8195c5
Shashank <shashanksaxena226@gmail.com>
2022-08-27 22:21:19 +0530

    Thread

--- Thread/Main.java
+++ Thread/Main.java
@@ -1,66 +1,12 @@
-
-class T extends Thread
+class Main
 {
-	public void run()
+	public static void main(String[] args) {
+		int a=10,b=20;
+		System.out.println(add(a,b));
+		System.out.println(add(30,40));
+	}
+	static int add(int a,int b)
 	{
-		for(int i=0;i<10;i++)
-		{
-			System.out.print(i+" ");
-			try{
-			Thread.sleep(1000);
-		}catch(Exception e)
-		{
-		    
-		}
-		}
-		
+		return (a+b);
 	}
 }
-class E implements Runnable
-{
-	public void run()
-	{
-		for(int i=0;i<10;i++)
-		{
-			System.out.print(i+" ");
-			try{
-			    Thread.sleep(1500);
-		        }
-		    catch(Exception e)
-	           {
-		    
-	           }
-		}
-		
-	}
-}
-
-class Main {
-	public static void main(String[] args)
-	{
-		T t1=new T();
-		t1.start();
-		E e=new E();
-		Thread t2=new Thread(e);
-		t2.start();
-		Thread t3=new Thread(new Runnable(){
-		  public void run()
-	        {
-		        for(int i=0;i<10;i++)
-		        {
-			        System.out.print(i+" ");
-			    try{
-			        Thread.sleep(1500);
-		        }catch(Exception e)
-        		{
-        		    
-        		}
-		        }
-		
-	        }});
-		    t3.start();
-		
-}
-}
-
-
