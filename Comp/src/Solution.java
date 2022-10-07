5f43cd9d46fe70623dbc7e236fbfe3a014e3a20b
Shashank <shashanksaxena226@gmail.com>
2022-10-07 11:00:57 +0530

    Comp

--- Comp/src/Solution.java
+++ Comp/src/Solution.java
@@ -1,4 +1,24 @@
+import java.util.*;
+
 class Solution
 {
+    public static void main(String[] args) {
+        pattern(5);
+
+    }
+    static void pattern(int n)
+    {
+        int b=1;
+        for(int i=1;i<=n;i++)
+        {
+            b=i%2==0?0:1;
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(b);
+                b=b==1?0:1;
+            }
+            System.out.println();
+        }
+    }
 
 }
